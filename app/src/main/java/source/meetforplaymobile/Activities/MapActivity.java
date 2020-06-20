a93c5f21ccb4caced538e3ce365f5c459c321017
MatGie <matgie9778@gmail.com>
2020-06-20 10:58:14 +0200

    wip

--- app/src/main/java/source/meetforplaymobile/Activities/MapActivity.java
+++ app/src/main/java/source/meetforplaymobile/Activities/MapActivity.java
@@ -1,42 +1,48 @@
 package source.meetforplaymobile.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-import androidx.appcompat.widget.Toolbar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import source.meetforplaymobile.Api.ApiResponseInterface;
 import source.meetforplaymobile.Api.RetrofitManager;
 import source.meetforplaymobile.Models.EventCoordinates;
-import source.meetforplaymobile.Models.User;
 import source.meetforplaymobile.R;
 
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity implements
+        OnMapReadyCallback {
+    AlertDialog.Builder builder;
     private int userId;
     private String userEmail;
     private TextView userEmailText;
     private GoogleMap map;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        Intent intent = getIntent();
+        userId = intent.getIntExtra("userId", 0);
+        userEmail = intent.getStringExtra("userEmail");
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -44,40 +50,70 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mapFragment.getMapAsync(this);
     }
 
-
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
         RetrofitManager retrofitManager = new RetrofitManager();
 
-        retrofitManager.getData("GetEventsCoordinates",null, new ApiResponseInterface() {
+        builder = new AlertDialog.Builder(this);
+
+        retrofitManager.getData("GetEventsCoordinates", null, new ApiResponseInterface() {
             @Override
             public void onSuccess(@NonNull String value) {
 
-                List<EventCoordinates> EventCoordinates_list = new Gson().fromJson(
+                final List<EventCoordinates> eventCoordinatesList = new Gson().fromJson(
                         value, new TypeToken<List<EventCoordinates>>() {
                         }.getType());
 
 
-
                 float zoomLevel = (float) 6.0;
                 map = googleMap;
-                LatLng poland= new LatLng(51.755482, 19.363078);
+                LatLng poland = new LatLng(51.755482, 19.363078);
                 map.moveCamera(CameraUpdateFactory.newLatLngZoom(poland, zoomLevel));
 
-                for (int i = 0; i< EventCoordinates_list.size(); i++) {
-
-                    double latitude = EventCoordinates_list.get(i).getLatitude();
-                    double longitude = EventCoordinates_list.get(i).getLongitude();
-                    String title = EventCoordinates_list.get(i).getEventName();
+                for (int i = 0; i < eventCoordinatesList.size(); i++) {
+                    double latitude = eventCoordinatesList.get(i).getLatitude();
+                    double longitude = eventCoordinatesList.get(i).getLongitude();
                     LatLng point = new LatLng(latitude, longitude);
-                    map.addMarker(new MarkerOptions().position(point).title(title));
-
-
+                    map.addMarker(new MarkerOptions().position(point)).setTag(0);
                 }
 
+                map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
 
+                        EventCoordinates event = eventCoordinatesList.stream().filter(
+                                e -> e.getLatitude() == marker.getPosition().latitude
+                                        && e.getLongitude() == marker.getPosition().longitude
+                        ).findFirst().orElse(null);
+                        assert event != null;
 
+                        builder.setMessage("Do you want to close this application ?")
+                                .setCancelable(true)
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        finish();
+                                        Toast.makeText(getApplicationContext(), "you choose yes action for alertbox",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        //  Action for 'NO' Button
+                                        dialog.cancel();
+                                        Toast.makeText(getApplicationContext(), "you choose no action for alertbox",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                        AlertDialog dialog = builder.create();
+
+                        dialog.setTitle(event.getEventName());
+                        dialog.show();
+                        return true;
+                    }
+
+                    ;
+                });
             }
 
             @Override
@@ -85,10 +121,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
             }
         });
-
-
-
-
-
     }
+
+
 }
